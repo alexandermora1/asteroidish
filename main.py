495d045d9a6950b05f8ba7594c409ca292901e5d
alexandermora1 <alexandermora@live.no>
2025-08-25 17:39:56 +0200

    Refactor using groups

--- main.py
+++ main.py
@@ -4,25 +4,37 @@ from player import Player
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
+    Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+        
 
     while True:
         for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-              return
-        screen.fill((0,0,0))
+            if event.type == pygame.QUIT:
+                return
+        
         dt = clock.tick(60) / 1000
-        player.update(dt)
-        player.draw(screen)
+
+        updateable.update(dt)
+
+        screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip() #needs to be last
 
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
